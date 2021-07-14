3430c42910da2adaf5aaf987007d1d9acbb1f75c
Shohei Sato <shoheisato12@gmail.com>
2021-07-14 10:35:42 -0300

    .

--- C++/source/LyapExp.cpp
+++ C++/source/LyapExp.cpp
@@ -14,47 +14,50 @@ std::vector<double> LyapunovExponents (std::vector<double>(*function)(std::vecto
     {
         coord[l]=rungeKutta4thSquare(function, coord[l-1], 1e-3, step, 4);
         //Normalização do primeiro vetor
-            for(int i=0;i<NumEq;i++)
+       /* for(int i=0;i<NumEq;i++)
+        {
+            norm[0]+=pow(coord[l][NumEq*i+1],2);
+        }
+        norm[0]=sqrt(norm[0]);
+        for(int i=0;i<NumEq;i++)
+        {
+            coord[l][NumEq*i+1]/=norm[0];        
+        }*/
+        norm[0]+=pow(coord[l][0],2);
+        norm[0]=sqrt(norm[0]);
+        coord[l][0]/=norm[0];        
+        //Normalização dos demais vetores
+        for(int i=1;i<NumEq;i++)
+        {
+            for(int j=0;j<i-1;j++)
             {
-                norm[0]+=pow(coord[l][NumEq*i+1],2);
+                for(int k=0;k<NumEq;k++)
+                {
+                    GramCoef[j]+=coord[l][NumEq*k+i]*coord[l][NumEq*k+j];
+                }
             }
-            norm[0]=sqrt(norm[0]);
-            for(int i=0;i<NumEq;i++)
+            for(int j=0;j<NumEq;j++)
             {
-                coord[l][NumEq*i+1]/=norm[0];        
+                for(int k=0;k<i-1;k++)
+                {
+                    coord[l][NumEq*j+i]-=GramCoef[k]*coord[l][NumEq*j+i];
+                }
             }
-            //Normalização dos demais vetores
-            for(int i=1;i<NumEq;i++)
+            for(int j=0;j<NumEq;j++)
             {
-                for(int j=0;j<i-1;j++)
-                {
-                    for(int k=0;k<NumEq;k++)
-                    {
-                        GramCoef[j]+=coord[l][NumEq*k+i]*coord[l][NumEq*k+j];
-                    }
-                }
-                for(int j=0;j<NumEq;j++)
-                {
-                    for(int k=0;k<i-1;k++)
-                    {
-                        coord[l][NumEq*j+i]-=GramCoef[k]*coord[l][NumEq*j+i];
-                    }
-                }
-                for(int j=0;j<NumEq;j++)
-                {
-                    norm[i]+=pow(coord[l][NumEq*j+i],2);
-                }
-                norm[i]=sqrt(norm[i]);
-                for(int j=0;j<NumEq;j++)
-                {
-                    coord[l][NumEq*j+i]*=norm[i];
-                }
-                //Cálculo de expoente de Lypunov
-                for(int j=0;j<NumEq;j++)
-                {
-                    exponents[j]=+log(norm[j])/log(2.0);
-                }
-                std::cout<<coord[l][0]<<"    "<<exponents[0]/coord[l][0]
+                norm[i]+=pow(coord[l][NumEq*j+i],2);
+            }
+            norm[i]=sqrt(norm[i]);
+            for(int j=0;j<NumEq;j++)
+            {
+                coord[l][NumEq*j+i]*=norm[i];
+            }
+            //Cálculo de expoente de Lypunov
+            for(int j=0;j<NumEq;j++)
+            {
+                exponents[j]=+log(norm[j])/log(2.0);
+            }
+            std::cout<<coord[l][0]<<"    "<<exponents[0]/coord[l][0]
                                     <<"    "<<exponents[1]/coord[l][0]
                                     <<"    "<<exponents[2]/coord[l][0]
                                     <<"    "<<exponents[3]/coord[l][0]<<std::endl;
